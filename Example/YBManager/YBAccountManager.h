44b72bf02a2dfe7c7bca0166c4325fafbda177fc
yan <476018863@qq.com>
2019-12-24 13:43:28 +0800

    0.1.6

---
+++ Example/YBManager/YBAccountManager.h
@@ -0,0 +1,17 @@
+//
+//  YBAccountManager.h
+//  YBManager_Example
+//
+//  Created by 氧车乐 on 2019/12/24.
+//  Copyright © 2019 476018863@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YBAccountManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
