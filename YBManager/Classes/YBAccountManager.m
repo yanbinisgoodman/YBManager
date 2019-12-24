bdd6b5a4c2a4ac179c85bba8acd27cd309566725
yan <476018863@qq.com>
2019-12-24 13:47:55 +0800

    0.1.7

---
+++ YBManager/Classes/YBAccountManager.m
@@ -0,0 +1,13 @@
+//
+//  YBAccountManager.m
+//  YBManager_Example
+//
+//  Created by 氧车乐 on 2019/12/24.
+//  Copyright © 2019 476018863@qq.com. All rights reserved.
+//
+
+#import "YBAccountManager.h"
+
+@implementation YBAccountManager
+
+@end
